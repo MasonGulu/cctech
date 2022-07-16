ae4406d0a321e2388b3cff5741b762fcaa7745b8
Mason Gulu <mason.gulu@gmail.com>
2022-07-15 21:04:14 -0400

    Added basic completion to the tape program

---
+++ src/main/resources/data/computercraft/lua/rom/autorun/cctech_completion.lua
@@ -0,0 +1,7 @@
+local completion = require("cc.completion")
+
+shell.setCompletionFunction("rom/programs/tape.lua", function(shell, nIndex, sText, prev)
+  if nIndex == 1 then
+    return completion.choice(sText, {"load", "save", "delete", "seek", "list", "wipe"}, true)
+  end
+end)
