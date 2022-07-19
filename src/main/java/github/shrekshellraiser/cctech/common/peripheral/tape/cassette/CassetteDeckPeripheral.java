6780ac1c8a9578f4a312470a6f251777f2105e1a
Mason Gulu <mason.gulu@gmail.com>
2022-07-19 02:19:35 -0400

    Generalized tape peripheral

--- src/main/java/github/shrekshellraiser/cctech/common/peripheral/tape/cassette/CassetteDeckPeripheral.java
+++ src/main/java/github/shrekshellraiser/cctech/common/peripheral/tape/cassette/CassetteDeckPeripheral.java
@@ -4,79 +4,21 @@ import dan200.computercraft.api.lua.LuaFunction;
 import dan200.computercraft.api.peripheral.IComputerAccess;
 import dan200.computercraft.api.peripheral.IPeripheral;
 import github.shrekshellraiser.cctech.common.item.tape.CassetteItem;
+import github.shrekshellraiser.cctech.common.peripheral.tape.TapePeripheral;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CassetteDeckPeripheral implements IPeripheral {
-
-    private final List<IComputerAccess> connectedComputers = new ArrayList<>();
-
-    private final CassetteDeckBlockEntity tileEntity;
-
+public class CassetteDeckPeripheral extends TapePeripheral {
     public CassetteDeckPeripheral(CassetteDeckBlockEntity tileEntity) {
-        this.tileEntity = tileEntity;
+        super(tileEntity);
     }
 
     @NotNull
     @Override
     public String getType() {
         return "cassette_deck";
-    }
-
-    @Override
-    public boolean equals(IPeripheral other) {
-        return this == other;
-    }
-
-    @Override
-    public void detach(@Nonnull IComputerAccess computer) {
-        connectedComputers.remove(computer);
-    }
-
-    @Override
-    public void attach(@Nonnull IComputerAccess computer) {
-        connectedComputers.add(computer);
-    }
-
-    @LuaFunction
-    public final boolean hasTape() {
-        return tileEntity.getItem() instanceof CassetteItem;
-    }
-
-    @LuaFunction
-    public final String read(int characters) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < characters; i++) {
-            String readChar = tileEntity.readChar();
-            stringBuilder.append(readChar);
-            if (readChar.equals("")) break;
-        }
-        return stringBuilder.toString();
-    }
-
-    @LuaFunction
-    public final boolean write(String ch) {
-        char[] chars = ch.toCharArray();
-        for (char c : chars) {
-            if (!tileEntity.writeChar(c))
-                return false;
-        }
-        return true;
-    }
-
-    @LuaFunction
-    public final boolean seek(int offset) {
-        return tileEntity.seekRel(offset);
-    }
-    @LuaFunction
-    public final boolean setLabel(String label) {
-        return tileEntity.setLabel(label);
-    }
-    @LuaFunction
-    public final boolean clearLabel() {
-        return tileEntity.clearLabel();
     }
 }
--- src/main/java/github/shrekshellraiser/cctech/common/peripheral/tape/reel/ReelToReelPeripheral.java
+++ src/main/java/github/shrekshellraiser/cctech/common/peripheral/tape/reel/ReelToReelPeripheral.java
@@ -4,70 +4,25 @@ import dan200.computercraft.api.lua.LuaFunction;
 import dan200.computercraft.api.peripheral.IComputerAccess;
 import dan200.computercraft.api.peripheral.IPeripheral;
 import github.shrekshellraiser.cctech.common.item.tape.ReelItem;
+import github.shrekshellraiser.cctech.common.peripheral.tape.TapeBlockEntity;
+import github.shrekshellraiser.cctech.common.peripheral.tape.TapePeripheral;
+import github.shrekshellraiser.cctech.common.peripheral.tape.cassette.CassetteDeckBlockEntity;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ReelToReelPeripheral implements IPeripheral {
+public class ReelToReelPeripheral extends TapePeripheral {
 
-    private final List<IComputerAccess> connectedComputers = new ArrayList<>();
-
-    private final ReelToReelBlockEntity tileEntity;
-
-    public ReelToReelPeripheral(ReelToReelBlockEntity tileEntity) {
-        this.tileEntity = tileEntity;
+    public ReelToReelPeripheral(TapeBlockEntity tileEntity) {
+        super(tileEntity);
     }
 
     @NotNull
     @Override
     public String getType() {
         return "reel_to_reel";
-    }
-
-    @Override
-    public boolean equals(IPeripheral other) {
-        return this == other;
-    }
-
-    @Override
-    public void detach(@Nonnull IComputerAccess computer) {
-        connectedComputers.remove(computer);
-    }
-
-    @Override
-    public void attach(@Nonnull IComputerAccess computer) {
-        connectedComputers.add(computer);
-    }
-
-    @LuaFunction
-    public final boolean hasTape() {
-        return tileEntity.getItem() instanceof ReelItem;
-    }
-
-    @LuaFunction
-    public final String read(int characters) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < characters; i++) {
-            stringBuilder.append(tileEntity.readChar());
-        }
-        return stringBuilder.toString();
-    }
-
-    @LuaFunction
-    public final boolean write(String ch) {
-        char[] chars = ch.toCharArray();
-        for (char c : chars) {
-            if (!tileEntity.writeChar(c))
-                return false;
-        }
-        return true;
-    }
-
-    @LuaFunction
-    public final boolean seek(int offset) {
-        return tileEntity.seekRel(offset);
     }
 
     @LuaFunction
@@ -77,14 +32,6 @@ public class ReelToReelPeripheral implements IPeripheral {
 
     @LuaFunction
     public final int getPos() {
-        return tileEntity.getPointer();
-    }
-    @LuaFunction
-    public final boolean setLabel(String label) {
-        return tileEntity.setLabel(label);
-    }
-    @LuaFunction
-    public final boolean clearLabel() {
-        return tileEntity.clearLabel();
+        return ((ReelToReelBlockEntity)tileEntity).getPointer();
     }
 }
